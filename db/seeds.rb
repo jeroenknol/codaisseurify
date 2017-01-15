7bb0c3150a7ce4f932bc802b50097c0f6e2a228f
jeroenknol <jeroen-knol@live.nl>
2017-01-15 12:37:33 +0100

    Add seed to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Artist.delete_all
+Song.delete_all
+
+song1 = Song.create(title: "Peace of mind")
+song2 = Song.create(title: "Another chance")
+song3 = Song.create(title: "We're all we need")
+song4 = Song.create(title: "Celestial")
+song5 = Song.create(title: "Super Human")
+song6 = Song.create(title: "Tommorow Boys")
+song7 = Song.create(title: "Hey now")
+song8 = Song.create(title: "Strong")
+song9 = Song.create(title: "Wasting my young years")
+
+artist1 = Artist.create(name: "Above & Beyond")
+artist2 = Artist.create(name: "Andrew Bayer")
+artist3 = Artist.create(name: "London Grammar")
+
+artist1.songs << [song1]
+artist1.songs << [song2]
+artist1.songs << [song3]
+artist1.save
+
+artist2.songs << [song4]
+artist2.songs << [song5]
+artist2.songs << [song6]
+artist2.save
+
+artist3.songs << [song7]
+artist3.songs << [song8]
+artist3.songs << [song9]
+artist3.save
