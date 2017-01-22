88b6ea3f3f3e73748e00fcf2975823ea909393a9
jeroenknol <jeroen-knol@live.nl>
2017-01-22 14:18:43 +0100

    Trying to add photos and added link to artist show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
+Photo.delete_all
 Song.delete_all
 Artist.delete_all
 
+# PICTURES
+photo1 = Photo.create(remote_image_url: "http://res.cloudinary.com/dfdlufyaj/image/upload/v1485086792/aboveAndBeyond_nqbi9t.jpg")
+photo2 = Photo.create(remote_image_url: "http://res.cloudinary.com/dfdlufyaj/image/upload/v1485086792/AndrewBayer_ssptkt.jpg")
+photo3 = Photo.create(remote_image_url: "http://res.cloudinary.com/dfdlufyaj/image/upload/v1485086792/LondonGrammar_ai8gzw.jpg")
+
+# SONGS
 song1 = Song.create(title: "Peace of mind")
 song2 = Song.create(title: "Another chance")
 song3 = Song.create(title: "We're all we need")
@@ -11,9 +18,10 @@ song7 = Song.create(title: "Hey now")
 song8 = Song.create(title: "Strong")
 song9 = Song.create(title: "Wasting my young years")
 
-artist1 = Artist.create(name: "Above & Beyond")
-artist2 = Artist.create(name: "Andrew Bayer")
-artist3 = Artist.create(name: "London Grammar")
+#ARTISTS
+artist1 = Artist.create(name: "Above & Beyond", photo: [photo1])
+artist2 = Artist.create(name: "Andrew Bayer", photo: [photo2])
+artist3 = Artist.create(name: "London Grammar", photo: [photo3])
 
 artist1.songs << [song1]
 artist1.songs << [song2]
